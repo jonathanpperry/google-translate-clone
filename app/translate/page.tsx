83d43e371d3d572853583ea0c3359273e67852e9
Jonathan Perry <jpper@umich.edu>
2024-10-17 23:05:01 -0500

    Finish building the translation form component

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,11 +1,41 @@
+import TranslationForm from "@/components/TranslationForm";
 import { auth } from "@clerk/nextjs/server";
 
-const TranslatePage = () => {
+export type TranslationLanguages = {
+  translation: {
+    [key: string]: {
+      name: string;
+      nativeName: string;
+      dir: "ltr" | "rtl";
+    };
+  };
+};
+
+const TranslatePage = async () => {
   auth().protect();
 
   const { userId } = auth();
+  if (!userId) throw new Error("User not logged in");
 
-  return <div>TranslatePage: {userId}</div>;
+  const languagesEndpoint =
+    "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0";
+
+  const response = await fetch(languagesEndpoint, {
+    next: {
+      // Cache results for 24 hours, then refresh
+      revalidate: 60 * 60 * 24,
+    },
+  });
+
+  const languages = (await response.json()) as TranslationLanguages;
+
+  return (
+    <div className="px-10 xl:px-0 mb-20">
+      <TranslationForm languages={languages} />
+
+      {/* Translation history */}
+    </div>
+  );
 };
 
 export default TranslatePage;
---
+++ components/TranslationForm.tsx
@@ -0,0 +1,156 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+import { TranslationLanguages } from "@/app/translate/page";
+import { useEffect, useRef, useState } from "react";
+import { useFormState } from "react-dom";
+import translate from "@/actions/translate";
+import Image from "next/image";
+
+const initialState = {
+  inputLanguage: "auto",
+  input: "",
+  outputLanguage: "es",
+  output: "",
+};
+
+export type State = typeof initialState;
+
+const TranslationForm = ({
+  languages,
+}: {
+  languages: TranslationLanguages;
+}) => {
+  const [state, formAction] = useFormState(translate, initialState);
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const submitBtnRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (!input.trim()) return;
+
+    const delayDebounceFn = setTimeout(() => {
+      // Submit the form
+      submitBtnRef.current?.click();
+
+      return () => clearTimeout(delayDebounceFn);
+    }, 500);
+  }, [input]);
+
+  useEffect(() => {
+    if (state.output) {
+      setOutput(state.output);
+    }
+  }, [state]);
+
+  return (
+    <div>
+      <div className="flex space-x-2">
+        <div className="flex items-center group cursor-pointer border rounded-md w-fit px-3 py-2 bg-[#E7F0FE] mb-5">
+          <Image
+            src="https://links.papareact.com/r9c"
+            alt="logo"
+            width={30}
+            height={30}
+          />
+          <p className="text-sm font-medium text-blue-500 group-hover:underline ml-2 mt-1">
+            Text
+          </p>
+        </div>
+
+        {/* Recorder */}
+      </div>
+
+      <form action={formAction}>
+        <div className="flex flex-col space-y-2 lg:flex-row lg:space-y-0 lg:space-x-2">
+          <div className="flex-1 space-y-2">
+            <Select name="inputLanguage" defaultValue="auto">
+              <SelectTrigger className="w-[280px] border-none text-blue-500 font-bold">
+                <SelectValue placeholder="Select a language" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Want us to figure it out?</SelectLabel>
+
+                  <SelectItem key="auto" value="auto">
+                    Auto-Detection
+                  </SelectItem>
+                </SelectGroup>
+                <SelectGroup>
+                  <SelectLabel>Language</SelectLabel>
+
+                  {Object.entries(languages.translation).map(([key, value]) => (
+                    <SelectItem key={key} value={key}>
+                      {value.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Textarea
+              placeholder="Type your message here."
+              className="min-h-32 text-xl"
+              name="input"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+
+          <div className="flex-1 space-y-2">
+            <Select name="outputLanguage" defaultValue="es">
+              <SelectTrigger className="w-[280px] border-none text-blue-500 font-bold">
+                <SelectValue placeholder="Select a language" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Want us to figure it out?</SelectLabel>
+
+                  <SelectItem key="auto" value="auto">
+                    Auto-Detection
+                  </SelectItem>
+                </SelectGroup>
+                <SelectGroup>
+                  <SelectLabel>Language</SelectLabel>
+
+                  {Object.entries(languages.translation).map(([key, value]) => (
+                    <SelectItem key={key} value={key}>
+                      {value.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Textarea
+              placeholder="Type your message here."
+              className="min-h-32 text-xl"
+              name="output"
+              value={output}
+              onChange={(e) => setOutput(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" ref={submitBtnRef}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TranslationForm;
